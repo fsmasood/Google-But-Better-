76728447196e3f108e7d2e5225b2f89681f3d321
Farhan <fsmasood@mun.ca>
2023-02-05 19:28:36 -0330

    initial commit

---
+++ settings.py
@@ -0,0 +1,6 @@
+SEARCH_KEY = "AIzaSyA2jlbii5-naUKfxlrrdrOiimapyoYlvoM"
+SEARCH_ID = "e30a32b91d21a4751"
+COUNTRY = "ca"
+
+SEARCH_URL = "https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={query}&start={start}&num=10&gl=" + COUNTRY
+RESULT_COUNT = 20
