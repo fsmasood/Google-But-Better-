c462b9e12963c90099d63d83b91097b686d5f4af
fsmasood <107145145+fsmasood@users.noreply.github.com>
2023-02-05 19:32:44 -0330

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
-SEARCH_KEY = "AIzaSyA2jlbii5-naUKfxlrrdrOiimapyoYlvoM"
-SEARCH_ID = "e30a32b91d21a4751"
+SEARCH_KEY = "xxx"
+SEARCH_ID = "xxx"
 COUNTRY = "ca"
 
 SEARCH_URL = "https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={query}&start={start}&num=10&gl=" + COUNTRY
